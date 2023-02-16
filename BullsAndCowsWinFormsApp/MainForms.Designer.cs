86770e0de7586444c77dba00821bdf3d0f7d242d
Petru4e <petru4e85@mail.ru>
2023-02-16 14:30:53 +0300

    Добавьте файлы проекта.

---
+++ BullsAndCowsWinFormsApp/MainForms.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace BullsAndCowsWinFormsApp
+{
+    partial class MainForms
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.puzzleWordLable = new System.Windows.Forms.Label();
+            this.userWordTexBox = new System.Windows.Forms.TextBox();
+            this.bullsCountLabel = new System.Windows.Forms.Label();
+            this.cowsCountLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.historyDataGridView = new System.Windows.Forms.DataGridView();
+            this.StepColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NumberColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BullsCountColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CowsCountColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рестартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.historyDataGridView)).BeginInit();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // puzzleWordLable
+            // 
+            this.puzzleWordLable.AutoSize = true;
+            this.puzzleWordLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.puzzleWordLable.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.puzzleWordLable.Location = new System.Drawing.Point(30, 603);
+            this.puzzleWordLable.Name = "puzzleWordLable";
+            this.puzzleWordLable.Size = new System.Drawing.Size(70, 25);
+            this.puzzleWordLable.TabIndex = 0;
+            this.puzzleWordLable.Text = "label1";
+            // 
+            // userWordTexBox
+            // 
+            this.userWordTexBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userWordTexBox.Location = new System.Drawing.Point(17, 121);
+            this.userWordTexBox.Name = "userWordTexBox";
+            this.userWordTexBox.Size = new System.Drawing.Size(100, 34);
+            this.userWordTexBox.TabIndex = 1;
+            // 
+            // bullsCountLabel
+            // 
+            this.bullsCountLabel.AutoSize = true;
+            this.bullsCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bullsCountLabel.Location = new System.Drawing.Point(12, 353);
+            this.bullsCountLabel.Name = "bullsCountLabel";
+            this.bullsCountLabel.Size = new System.Drawing.Size(132, 29);
+            this.bullsCountLabel.TabIndex = 2;
+            this.bullsCountLabel.Text = "Быков = 0";
+            // 
+            // cowsCountLabel
+            // 
+            this.cowsCountLabel.AutoSize = true;
+            this.cowsCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cowsCountLabel.Location = new System.Drawing.Point(12, 440);
+            this.cowsCountLabel.Name = "cowsCountLabel";
+            this.cowsCountLabel.Size = new System.Drawing.Size(132, 29);
+            this.cowsCountLabel.TabIndex = 3;
+            this.cowsCountLabel.Text = "Коров = 0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(17, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // historyDataGridView
+            // 
+            this.historyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.historyDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.StepColumn,
+            this.NumberColumn,
+            this.BullsCountColumn,
+            this.CowsCountColumn});
+            this.historyDataGridView.Location = new System.Drawing.Point(265, 12);
+            this.historyDataGridView.Name = "historyDataGridView";
+            this.historyDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.historyDataGridView.RowTemplate.Height = 24;
+            this.historyDataGridView.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.historyDataGridView.Size = new System.Drawing.Size(725, 573);
+            this.historyDataGridView.TabIndex = 5;
+            // 
+            // StepColumn
+            // 
+            this.StepColumn.HeaderText = "Ход";
+            this.StepColumn.MinimumWidth = 6;
+            this.StepColumn.Name = "StepColumn";
+            this.StepColumn.Width = 125;
+            // 
+            // NumberColumn
+            // 
+            this.NumberColumn.HeaderText = "Число";
+            this.NumberColumn.MinimumWidth = 6;
+            this.NumberColumn.Name = "NumberColumn";
+            this.NumberColumn.Width = 125;
+            // 
+            // BullsCountColumn
+            // 
+            this.BullsCountColumn.HeaderText = "Быков";
+            this.BullsCountColumn.MinimumWidth = 6;
+            this.BullsCountColumn.Name = "BullsCountColumn";
+            this.BullsCountColumn.Width = 125;
+            // 
+            // CowsCountColumn
+            // 
+            this.CowsCountColumn.HeaderText = "Коров";
+            this.CowsCountColumn.MinimumWidth = 6;
+            this.CowsCountColumn.Name = "CowsCountColumn";
+            this.CowsCountColumn.Width = 125;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 28);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1082, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1082, 28);
+            this.menuStrip2.TabIndex = 7;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рестартToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 26);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // рестартToolStripMenuItem
+            // 
+            this.рестартToolStripMenuItem.Name = "рестартToolStripMenuItem";
+            this.рестартToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.рестартToolStripMenuItem.Text = "Рестарт";
+            this.рестартToolStripMenuItem.Click += new System.EventHandler(this.рестартToolStripMenuItem_Click);
+            // 
+            // MainForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 653);
+            this.Controls.Add(this.historyDataGridView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cowsCountLabel);
+            this.Controls.Add(this.bullsCountLabel);
+            this.Controls.Add(this.userWordTexBox);
+            this.Controls.Add(this.puzzleWordLable);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForms";
+            this.Text = "Bulls and cows";
+            this.Load += new System.EventHandler(this.MainForms_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.historyDataGridView)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label puzzleWordLable;
+        private System.Windows.Forms.TextBox userWordTexBox;
+        private System.Windows.Forms.Label bullsCountLabel;
+        private System.Windows.Forms.Label cowsCountLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView historyDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StepColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumberColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BullsCountColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CowsCountColumn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рестартToolStripMenuItem;
+    }
+}
+
---
+++ BullsAndCowsWinFormsApp/MainForms.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BullsAndCowsWinFormsApp
+{
+    public partial class MainForms : Form
+    {
+        string puzzleWord = "";
+        const int wordLength = 4;
+        int stepCount = 0;
+        public MainForms()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForms_Load(object sender, EventArgs e)
+        {
+            List<int> digits = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Random random = new Random();
+            for (int i = 0; i < wordLength; i++)
+            {
+                int digitIndex = random.Next(0, digits.Count);
+                puzzleWord += digits[digitIndex].ToString();
+                digits.RemoveAt(digitIndex);
+            }
+            puzzleWordLable.Text = puzzleWord;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userWord = userWordTexBox.Text;
+
+            if (!IsValid(userWord))
+            {
+                return;
+            }
+            stepCount++;
+            int bullsCount = CalculateBullsCount(userWord);
+            int cowsCount = CalculateCowsCount(userWord);
+
+            bullsCountLabel.Text = "Быков = " + bullsCount;
+            cowsCountLabel.Text = "Коров = " + cowsCount;
+
+            historyDataGridView.Rows.Add(stepCount, userWord, bullsCount, cowsCount);
+
+            if(bullsCount==wordLength)
+            {
+                MessageBox.Show($"Ура! Вы победили за {stepCount} шагов");
+                button1.Enabled = false;
+                userWordTexBox.Enabled = false;
+            }
+        }
+
+        private bool IsValid(string userWord)
+        {
+            if (userWord.Length != wordLength)
+            {
+                MessageBox.Show("Строка должна быть длинной 4!");
+                return false;
+            }
+
+            for (int i = 0; i < userWord.Length; i++)
+            {
+                if(!char.IsDigit(userWord[i]))
+                {
+                    MessageBox.Show("Строка должна содержать только цифры!");
+                    return false;
+                }
+            }
+            for (int i = 0; i < userWord.Length; i++)
+            {
+                for (int j = i + 1; j < userWord.Length; j++)
+                {
+                    if (userWord[i] == userWord[j])
+                    {
+                        MessageBox.Show("Строка должна содержать уникальные цифры!");
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private int CalculateBullsCount(string userWord)
+        {
+            int bullsCount = 0;
+            for (int i = 0; i < wordLength; i++)
+            {
+                if (puzzleWord[i] == userWord[i])
+                {
+                    bullsCount++;
+                }
+            }
+            return bullsCount;
+        }
+
+        private int CalculateCowsCount(string userWord)
+        {
+            int cowsCount = 0;
+            for (int i = 0; i < wordLength; i++)
+            {
+                for (int j = 0; j < wordLength; j++)
+                {
+                    if (i == j) continue;
+                    if (puzzleWord[i] == userWord[j])
+                    {
+                        cowsCount++;
+                    }
+
+                }
+            }
+            return cowsCount;
+        }
+
+        private void рестартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
